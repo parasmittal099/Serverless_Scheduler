132f1300c6b1945cc580cd20ef30a5d1e6ccdcd0
Aalhad Sawane <65013445+aalhadsawane@users.noreply.github.com>
2024-04-01 13:47:48 +0530

    recursive bruteforce as a class

--- mincost.py
+++ mincost.py
@@ -1,5 +1,5 @@
 import pulp
-
+import time
 # Linear programming in essence is giving some constraint curves which set an area on the cartesian plane,
 # now we have an objective function to maximise/minimise and this function/curve will be max/min at the vertices of the area.
 # This is a topic from 12th CBSE maths see graph graph below for a visual proof.
@@ -15,11 +15,11 @@ import pulp
 # returns 2 values: 1. a dict with keys as jobs and values as providers for the min cost combination
 #                   2. total cost (including delays) of the min cost combination
 def minimize_total_cost(workers, jobs, cost_matrix, delay):
-
+    l = time.time()
     # Create a binary variable for each combination of worker and job
-    # this denotes if that combination is assigned (1) or not (0) p.s. 'assign' below is just a label.
+    # this denotes if that combination of worker and job is chosen (1) or not (0) p.s. 'chosen' below is just a label.
     # for each job there is one worker who has value(x[worker,job]==1 and rest all workers for that job have x == 0)
-    x = pulp.LpVariable.dicts('assign', ((worker, job) for worker in workers for job in jobs), cat='Binary')
+    x = pulp.LpVariable.dicts('chosen', ((worker, job) for worker in workers for job in jobs), cat='Binary')
     
     # pulp.LpMinimize will minimise the objective function
     prob = pulp.LpProblem("Minimize_Total_Cost", pulp.LpMinimize)
@@ -28,7 +28,11 @@ def minimize_total_cost(workers, jobs, cost_matrix, delay):
     # Objective function: total cost of runtimes + delay once per worker
     # Objective function is the expression to minimise/maximise
     # for loops are basically summation symbols here since it is inside lpSum.
-    prob += pulp.lpSum(x[worker, job] * cost_matrix[worker][job] for worker in workers for job in jobs) + pulp.lpSum(delay[worker] * pulp.lpSum(x[worker, job] for job in jobs) for worker in workers)
+    # the if statement are so that 1 taken because if a worker has taken more than 1 jobs it the delay should still
+    # be multiplied with 1 and not the number of jobs.
+
+    prob += pulp.lpSum(x[worker, job] * cost_matrix[worker][job] for worker in workers for job in jobs) + pulp.lpSum(delay[worker] * (pulp.lpSum(x[(worker, job)] for job in jobs) if pulp.lpSum(x[(worker, job)] for job in jobs) <= 1 else 1) for worker in workers)
+
     
     # Constraints: each job must be assigned to exactly one worker
     # Constraints are inequalities or equalities instead of an expression
@@ -54,19 +58,32 @@ def minimize_total_cost(workers, jobs, cost_matrix, delay):
     total_cost = sum(cost_matrix[assignment[job]][job] for job in jobs)
     total_cost += sum(delay[worker] for worker in workers if any(pulp.value(x[worker, job]) == 1 for job in jobs))
     
+    print("Time taken in wallclock sec by lp algo: ", time.time()-l)
     return assignment, total_cost
 
 # Example usage
-providers = ['Provider1', 'Provider2']
-jobs = ['Job1', 'Job2', 'Job3']
-cost_matrix = {
-    'Provider1' : {'Job1': 20, 'Job2': 20, 'Job3': 30},
-    'Provider2' : {'Job1': 15, 'Job2': 15, 'Job3': 15}
-}
-delay = {'Provider1': 0, 'Provider2': 30}
+import random
 
-assignment, total_cost = minimize_total_cost(providers, jobs, cost_matrix, delay)
+# Generate workers and jobs
+workers = ['Worker1', 'Worker2', 'Worker3', 'Worker4', 'Worker5', 'Worker6']
+jobs = ['Job1', 'Job2', 'Job3', 'Job4', 'Job5', 'Job6', 'Job7', 'Job8', 'Job9', 'Job10']
+
+# Define fixed cost matrix
+cost_matrix = {
+    'Worker1': {'Job1': 10, 'Job2': 15, 'Job3': 20, 'Job4': 25, 'Job5': 30, 'Job6': 35, 'Job7': 40, 'Job8': 45, 'Job9': 50, 'Job10': 55},
+    'Worker2': {'Job1': 20, 'Job2': 25, 'Job3': 30, 'Job4': 35, 'Job5': 40, 'Job6': 45, 'Job7': 50, 'Job8': 55, 'Job9': 60, 'Job10': 65},
+    'Worker3': {'Job1': 30, 'Job2': 35, 'Job3': 40, 'Job4': 45, 'Job5': 50, 'Job6': 55, 'Job7': 60, 'Job8': 65, 'Job9': 70, 'Job10': 75},
+    'Worker4': {'Job1': 40, 'Job2': 45, 'Job3': 50, 'Job4': 55, 'Job5': 60, 'Job6': 65, 'Job7': 70, 'Job8': 75, 'Job9': 80, 'Job10': 85},
+    'Worker5': {'Job1': 50, 'Job2': 55, 'Job3': 60, 'Job4': 65, 'Job5': 70, 'Job6': 75, 'Job7': 80, 'Job8': 85, 'Job9': 90, 'Job10': 95},
+    'Worker6': {'Job1': 60, 'Job2': 65, 'Job3': 70, 'Job4': 75, 'Job5': 80, 'Job6': 85, 'Job7': 90, 'Job8': 95, 'Job9': 100, 'Job10': 105}
+}
+
+# Define fixed delay dictionary
+delay = {
+    'Worker1': 0, 'Worker2': 20, 'Worker3': 10, 'Worker4': 30, 'Worker5': 15, 'Worker6': 25
+}
+assignment, total_cost = minimize_total_cost(workers, jobs, cost_matrix, delay)
 
 print("Job Assignments:")
 print(assignment)
-print("\nTotal Cost (including delay):", total_cost)
+print("Total Cost (including delay):", total_cost)
--- mincostbruteforce.py
+++ mincostbruteforce.py
@@ -1,43 +1,64 @@
-min_cost_matrix=[]
-min_cost=float('inf')
-providers = ['A', 'B']
-accounted_for=[0,0]
-run_times = [
-    [20, 20, 30],  # Run times for provider A
-    [15, 15, 15],  # Run times for provider B   # Run times for provider C
-]
-delay = [0,30]
-job_identifiers = ['j1', 'j2', 'j3']
+import time
 
-cost_matrix = []
-
-for i, provider in enumerate(providers):
-    run_time_mapping = {job_identifiers[j]: run_times[i][j] for j in range(len(run_times[i]))}
-    cost_matrix.append({provider: run_time_mapping})
-def fun(current_job, current_matrix,current_cost):
-    if(current_job==len(job_identifiers)):
-        global min_cost
-        global min_cost_matrix
-        if(current_cost<min_cost):
-            min_cost=current_cost
-            #print(current_matrix)
-            min_cost_matrix=current_matrix.copy()
-            #print(min_cost_matrix)
-        return
-    for i,provider in enumerate(providers):
-        state=accounted_for[i]
-        if(state==0):
-            current_cost+=delay[i]
-            accounted_for[i]=1
-        mapping= {job_identifiers[current_job]:run_times[i][current_job]}
-        current_cost+=run_times[i][current_job]
-        current_matrix.append({provider: mapping})
-        fun(current_job+1,current_matrix,current_cost)
-        current_matrix.pop()
-        if(state==0):
-            accounted_for[i]=0
-            current_cost-=delay[i]
-        current_cost-=run_times[i][current_job]
-fun(0,[],0)
-print(min_cost_matrix)
-print(min_cost)
+class BruteForce:
+    def __init__(self,cost_matrix,delay, job_identifiers, providers):
+        self.cost_matrix=cost_matrix
+        self.delay=delay
+        self.min_cost=float('inf')
+        self.min_cost_matrix=[]
+        self.providers= providers
+        self.job_identifiers=job_identifiers
+    def func(self,current_job, current_matrix,current_cost,providers,accounted_for,run_times,job_identifiers,delays):
+        if(current_job==len(job_identifiers)):
+            if(current_cost<self.min_cost):
+                self.min_cost=current_cost
+                self.min_cost_matrix=current_matrix.copy()
+            return
+        for i,provider in enumerate(providers):
+            state=accounted_for[i]
+            if(state==0):
+                current_cost+=delays[i]
+                accounted_for[i]=1
+            mapping= {job_identifiers[current_job]:run_times[i][current_job]}
+            current_cost+=run_times[i][current_job]
+            current_matrix.append({provider: mapping})
+            self.func(current_job+1,current_matrix,current_cost,providers,accounted_for,run_times,job_identifiers,delays)
+            current_matrix.pop()
+            if(state==0):
+                accounted_for[i]=0
+                current_cost-=delays[i]
+            current_cost-=run_times[i][current_job]
+    def brute_force(self):
+        l = time.time()
+        accounted_for=[0]*len(self.cost_matrix)
+        run_times = []
+        for provider_data in self.cost_matrix.items():
+            run_times_row = []
+            for job_data in provider_data[1].items():
+                run_times_row.append(job_data[1])
+            run_times.append(run_times_row)
+        delays=[]
+        for d in self.delay.items():
+            delays.append(d[1])
+        self.func(0,[],0,self.providers,accounted_for,run_times,self.job_identifiers,delays)
+        print("Time taken for recursive brute force: ", time.time()-l)
+        return self.min_cost,self.min_cost_matrix
+    
+# Example usage
+providers = ['Worker1', 'Worker2', 'Worker3', 'Worker4', 'Worker5', 'Worker6']
+job_identifiers = ['Job1', 'Job2', 'Job3', 'Job4', 'Job5', 'Job6', 'Job7', 'Job8', 'Job9', 'Job10']
+cost_matrix = {
+    'Worker1': {'Job1': 10, 'Job2': 15, 'Job3': 20, 'Job4': 25, 'Job5': 30, 'Job6': 35, 'Job7': 40, 'Job8': 45, 'Job9': 50, 'Job10': 55},
+    'Worker2': {'Job1': 20, 'Job2': 25, 'Job3': 30, 'Job4': 35, 'Job5': 40, 'Job6': 45, 'Job7': 50, 'Job8': 55, 'Job9': 60, 'Job10': 65},
+    'Worker3': {'Job1': 30, 'Job2': 35, 'Job3': 40, 'Job4': 45, 'Job5': 50, 'Job6': 55, 'Job7': 60, 'Job8': 65, 'Job9': 70, 'Job10': 75},
+    'Worker4': {'Job1': 40, 'Job2': 45, 'Job3': 50, 'Job4': 55, 'Job5': 60, 'Job6': 65, 'Job7': 70, 'Job8': 75, 'Job9': 80, 'Job10': 85},
+    'Worker5': {'Job1': 50, 'Job2': 55, 'Job3': 60, 'Job4': 65, 'Job5': 70, 'Job6': 75, 'Job7': 80, 'Job8': 85, 'Job9': 90, 'Job10': 95},
+    'Worker6': {'Job1': 60, 'Job2': 65, 'Job3': 70, 'Job4': 75, 'Job5': 80, 'Job6': 85, 'Job7': 90, 'Job8': 95, 'Job9': 100, 'Job10': 105}
+}
+delay = {
+    'Worker1': 0, 'Worker2': 20, 'Worker3': 10, 'Worker4': 30, 'Worker5': 15, 'Worker6': 25
+}
+solver=BruteForce(cost_matrix,delay,job_identifiers,providers)
+total_cost,total_cost_matrix=solver.brute_force()
+print("Total cost: ",total_cost)
+print("Total cost matrix: ",total_cost_matrix)
